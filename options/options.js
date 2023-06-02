d2bbc58c80908bbb1c64d895eea0e832f315941a
Crowdbotics <team@crowdbotics.com>
2023-06-02 20:14:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rock-ola-song-selec-41195",
+  url: "https://rock-ola-song-selec-41195.botics.co",
+  api: "https://rock-ola-song-selec-41195.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
